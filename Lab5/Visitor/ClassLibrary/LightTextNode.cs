b06a08f9b1e3715e1b228376ae88cee9c62d647a
Ruslan Kuksa <ipz221_kro@student.ztu.edu.ua>
2024-05-18 19:17:06 +0300

    Visitor pattern

---
+++ Lab5/Visitor/ClassLibrary/LightTextNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ClassLibrary
+{
+    public class LightTextNode : ILightNode, ILightEntity
+    {
+        public string Text { get; set; }
+        public LightTextNode(string text)
+        {
+            Text = text;
+        }
+        public string ToHtml()
+        {
+            return Text;
+        }
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitLightTextNode(this);
+        }
+    }
+}
