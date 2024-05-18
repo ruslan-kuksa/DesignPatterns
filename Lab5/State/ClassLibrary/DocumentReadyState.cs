ecbd1e4a70883c3ea89c5336c0ccf74ffd7ac9f6
Ruslan Kuksa <ipz221_kro@student.ztu.edu.ua>
2024-05-18 03:16:45 +0300

    State pattern

---
+++ Lab5/State/ClassLibrary/DocumentReadyState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class DocumentReadyState : IDocumentState
+    {
+        public string Handle()
+        {
+            return "Document is ready";
+        }
+    }
+}
